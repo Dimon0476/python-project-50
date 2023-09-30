8801c82bff405f549801fd268517948497d93af7
Dimon0476 <dmitry.fodorov@gmail.com>
2023-09-30 10:39:37 +0300

    step_3

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+from gendiff.cli import parse
+
+
+def main():
+    print(args)
+    #print(
+    #    generate_diff(args.first_file, args.second_file, formater=args.format))
+
+
+if __name__ == '__main__':
+    main()
